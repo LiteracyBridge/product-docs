b698e5f85486eb4969d0d55cce7cdeb35e8ec3d2
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-05-02 18:06:01 +0000

    ci: update repo name

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,5 @@
 export default {
-	base: "/product-documtation/", // make sure this matches your repo name
+	base: "/product-docs/", // make sure this matches your repo name
 	title: "Product Documentation",
 	description: "Documentation for Amplion product",
 
