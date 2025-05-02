a056b665210e564df425851a0d08596dd4a4bd2c
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-05-02 14:15:26 +0000

    ci: update theme config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,38 @@
 export default {
-    base: '/product-documtation/', // make sure this matches your repo name
-    title: 'Product Documentation',
-    description: 'Documentation for Amplion product',
-  }
-  
+	base: "/product-documtation/", // make sure this matches your repo name
+	title: "Product Documentation",
+	description: "Documentation for Amplion product",
+
+	themeConfig: {
+    aside: true,
+    outline: 2,
+		sidebar: [
+			{
+				text: "Audio Content Manager",
+				items: [
+					// { text: 'Introduction', link: '/introduction' },
+				],
+			},
+		],
+    editLink: {
+      pattern: 'https://github.com/LiteracyBridge/product-documtation/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    },
+    lastUpdated: {
+      text: 'Updated at',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    },
+    docFooter: {
+      prev: 'Pagina prior',
+      next: 'Proxima pagina'
+    }
+	},
+
+};
